a6f6c919f71a402acfe2c49342dabf8d2e0867f0
iaadarshpatel <as7128@srmist.edu.in>
2023-10-01 01:48:49 +0530

    adad

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -81,6 +81,7 @@ const Footer = () => {
       <p class="col-lg-8 col-sm-12 footer-text m-0 text-white"> Copyright &copy; All rights reserved </p>
       <div className="row footer-bottom d-flex">
       <div className="col-lg-4 col-sm-12 footer-social">
+      <a href="#"><FiTwitter/></a>
       <a href="#"><FaFacebookF/></a>
       <a href="#"><FaTwitter/></a>
       <a href="#"><FaLinkedin/></a>
